3276ff79b564c908e56f052980ca85cb58c52910
Mario <flonet.altervista@gmail.com>
2021-07-18 15:25:17 +0200

    sql
    aggiunti gli appuntamenti vaccinali (tolta la parte che identifica il vaccino ovviamente)
    ho cercato di renderli coerenti con quanto scritto sopra in modo da testare il maggior numero possibile di casi
    
    MA non potendo testare mi prendo il beneficio del dubbio, potrebbe esserci qualcosa di sbagliato, ma spero di no

--- dbinsert.sql
+++ dbinsert.sql
@@ -17,7 +17,7 @@ begin;
   insert into Cittadino
   value ('flpdfl95a11a123b', 'Filippo', 'De Filippi', 26, 'defilippi.fil@yahoo.com', null, 'Via de medici 90', 'Napoli', TRUE, 'altro');
   insert into Cittadino
-  value ('leizml70a11a123b', 'Elio', 'Zemello', 51, 'zemee51@libero.it', null, 'Via arsenale 5', 'Genova', FALSE, 'personale sanitario');
+  value ('leizml70a11a123b', 'Elio', 'Zemello', 51, 'zemee51@libero.it', null, 'Via arsenale 5', 'Milano', FALSE, 'personale sanitario');
   insert into Cittadino
   value ('brdstf86a11a123b', 'Berardo', 'Stefanelli', 35, null, 5645237982, 'Via garibaldi 10', 'Torino', TRUE, 'personale scolastico');
   insert into Cittadino
@@ -27,11 +27,17 @@ begin;
   insert into Cittadino
   value ('niamza69a11a123b', 'Iana', 'Mazzuia', 52, 'maziana_2@gmail.com', null, 'Strada della repubblica', 'Torino', FALSE, 'altro');
   insert into Cittadino
-  value ('piobrc85a11a123b', 'Pippo', 'Burchi', 36, null, 1236987459, 'Corso caselle 70', 'Roma', TRUE, 'personale sanitario');
+  value ('piobrc85a11a123b', 'Pippo', 'Burchi', 36, null, 1236987459, 'Corso caselle 70', 'Torino', TRUE, 'personale sanitario');
   insert into Cittadino
   value ('lnrfln66a11a123b', 'Leonardo', 'Filano', 55, 'fil.leo66@gmail.co,', null, 'Corso cesare 177', 'Milano', FALSE, 'personale sanitario');
   insert into Cittadino
   value ('rtidgz85a51a123b', 'Rita', 'De Guzzi', 36, null, 1596542587, 'Via dei mercanti 8', 'Genova', FALSE, 'altro');
+  insert into Cittadino
+  value ('fdamnt60a51a123b', 'Fadia', 'Menta', 61, 'f.menta@virgilio.it', null, 'Strada del colle 3', 'Roma', TRUE, 'personale sanitario');
+  insert into Cittadino
+  value ('antdgv70a11a123b', 'Antonio', 'De Giovanni', 51, null, 6549631478, 'Corso romano 75', 'Napoli', FALSE, 'personale sanitario');
+  insert into Cittadino
+  value ('mtacts89a11a123b', 'Matteo', 'Actis', 32, 'mattewactis@gmail.com', null, 'Via del mare 5', 'Genova', TRUE, 'personale sanitario');
 
 -- CentroVaccinale
   insert into CentroVaccinale
@@ -51,7 +57,13 @@ begin;
   insert into Medico
   value ('lnrfln66a11a123b', 'altro medico', 666666, TRUE);
   insert into Medico
-  value ('piobrc85a11a123b', 'altro medico', 111111, TRUE);
+  value ('piobrc85a11a123b', 'medico di base', 111111, TRUE);
+  insert into Medico
+  value ('fdamnt60a51a123b', 'altro medico', 333333, TRUE);
+  insert into Medico
+  value ('antdgv70a11a123b', 'altro medico', 444444, TRUE);
+  insert into Medico
+  value ('mtacts89a11a123b', 'altro medico', 555555, TRUE);
 
 -- Lotto
   insert into Lotto
@@ -112,6 +124,27 @@ begin;
 
 -- AppuntamentoVaccinale
 -- da rivedere
+-- idea possibile, se la dose di vaccino ha un id appuntamento vaccinale può avere una coppia tipo, id di attributi, dove id DEVE essere istanziato se la data odierna è successiva alla data dell'appuntamento
+  insert into AppuntamentoVaccinale
+  value ('2021-01-15', '09:00', 222222, 'leizml70a11a123b', '', 'lnismr60a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-02-20', '18:15', 222222, 'leizml70a11a123b', '', 'lnismr60a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-02-09', '15:30', 333333, 'fdamnt60a51a123b', '', 'nglghm98a51a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-07-1', '08:23', 222222, 'leizml70a11a123b', '', 'lnrfln66a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-03-03', '14:57', 555555, 'mtacts89a11a123b', '', 'rtidgz85a51a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-05-01', '15:45', 555555, 'mtacts89a11a123b', '', 'rtidgz85a51a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-04-30', '20:35', 111111, 'piobrc85a11a123b', '', 'fridpt11a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-01-7', '11:00', 111111, 'piobrc85a11a123b', '', 'brdstf86a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-06-15', '16:17', 444444, 'antdgv70a11a123b', '', 'gbrzpa91a11a123b');
+  insert into AppuntamentoVaccinale
+  value ('2021-07-15', '09:20', 444444, 'antdgv70a11a123b', '', 'gbrzpa91a11a123b');
 
 -- Possiede
 -- da rivedere
